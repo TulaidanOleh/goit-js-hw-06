e1865ff79b18950e9b36e35d916eff7b641e5504
Oleh Tulaidan <oleh.tulaidan@gmail.com>
2022-08-23 20:15:31 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listArrEl = ingredients.map((ingredient) => {
+  const listEl = document.createElement("li");
+  listEl.classList.add("item");
+  listEl.textContent = ingredient;
+  return listEl;
+});
+
+const ingredientsEl = document.querySelector("#ingredients");
+ingredientsEl.append(...listArrEl);
