d506f63389b974ee9a704d120aa2f920ee93e1c0
Oleh Tulaidan <oleh.tulaidan@gmail.com>
2022-08-28 17:37:04 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const loginFormSubmitEl = document.querySelector(".login-form");
 
+const user = {
+  email: "",
+  password: "",
+};
+
+const setOutput = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені !");
+  } else {
+    user.email = email.value;
+    user.password = password.value;
+    console.log(user.email, user.password);
+    loginFormSubmitEl.reset();
+  }
+};
+
+loginFormSubmitEl.addEventListener("submit", setOutput);
