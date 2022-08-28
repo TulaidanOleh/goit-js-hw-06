cff0a7549a343e385f04b1acc0f49fa0046ae687
Oleh Tulaidan <oleh.tulaidan@gmail.com>
2022-08-28 17:51:23 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+const buttonChangeColorEl = document.querySelector(".change-color");
+const nameColorEl = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  nameColorEl.textContent = color;
+}
+
+buttonChangeColorEl.addEventListener("click", changeColor);
